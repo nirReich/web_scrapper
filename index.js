3b8e4d1bd268a5532a4517465d7ccfdf10b7cf52
nirReich <reich.nir@gmail.com>
2021-10-07 13:01:37 +0300

    added error handeling

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const app = express();
 const webdriver = require("selenium-webdriver");
 
-// const url=" http://neurosymptomsnew.kk5.org/";
-
 const siteObj = { components: [] };
 
 const pushElementdata = (element, type, att, z_index, style = "none") => {
@@ -33,7 +31,8 @@ const getType = (className) => {
 
 //----------------------
 const runAuto = async (url) => {
-  let driver = await new webdriver.Builder().forBrowser("chrome").build();
+  try {
+    let driver = await new webdriver.Builder().forBrowser("chrome").build();
   let By = webdriver.By;
   await driver.get(url);
 
@@ -56,58 +55,59 @@ const runAuto = async (url) => {
   //get all inner html in text elements =============== keep on working!!!
   const editInnerHtml = async (htmlStr) => {
     try {
-
-    const newHtmlStr = htmlStr
-      .replace(/\n|\t|\r|\s/g, "")
-      .replace(/h1/g, "p ")
-      .replace(/(?<=class=).*?(?=\">)/g,"")
-      .replace(/class=\"/g,"")
-      .replace(/span/g, "span ");
+      const newHtmlStr = htmlStr
+        .replace(/\n|\t|\r|\s|(?<=class=).*?(?=\">)/g, "")
+        .replace(/h1/g, "p ")
+        .replace(/class=\"/g, "")
+        .replace(/span/g, "span ");
 
       return `<p>${newHtmlStr}</p>`;
     } catch (error) {
       console.log("error in editInnerHtml function: " + error);
     }
-
-
   };
 
   // handeling the element list
   async function elementsPush(list) {
-    for (let index = 0; index < list.length; index++) {
-      let eleData = "";
-      let eleStyle = "";
-      const elementRect = await list[index].getRect();
-      const eleType = getType(await list[index].getAttribute("class"));
-      const elementZ = await list[index].getCssValue("z-index");
-      // const innerHtmlList = await list[index].findElements(By.xpath(".//*"));
-      const innerHtml = await list[index].getAttribute("innerHTML")
-
-      const eleRgba = await list[index].getCssValue("background-color");
-
-      if (eleType === "title" || eleType === "body") {
-        eleData = { text:await editInnerHtml( innerHtml) };
-      } else if (eleType === "button") {
-        eleData = { href: await list[index].getAttribute("href") };
-        eleStyle = {
-          backgroundOpacity: eleRgba.slice(
-            eleRgba.length - 5,
-            eleRgba.length - 1
-          ),
-          backgroundColor: eleRgba,
-          backgroundImage: await list[index].getCssValue("background-image"),
-          borderColor: await list[index].getCssValue("background-color"),
-          borderWidth: await list[index].getCssValue("border-width"),
-          borderRadius: await list[index].getCssValue("border-radius"),
-          borderStyle: await list[index].getCssValue("border-style"),
-          shadow: await list[index].getCssValue("box-shadow"),
-        };
-      } else {
-        eleData = { src: await list[index].getAttribute("src") };
+    try {
+      for (let index = 0; index < list.length; index++) {
+        let eleData = "";
+        let eleStyle = "";
+        const elementRect = await list[index].getRect();
+        const eleType = getType(await list[index].getAttribute("class"));
+        const elementZ = await list[index].getCssValue("z-index");
+        // const innerHtmlList = await list[index].findElements(By.xpath(".//*"));
+        const innerHtml = await list[index].getAttribute("innerHTML");
+  
+        const eleRgba = await list[index].getCssValue("background-color");
+  
+        if (eleType === "title" || eleType === "body") {
+          eleData = { text: await editInnerHtml(innerHtml) };
+        } else if (eleType === "button") {
+          eleData = { href: await list[index].getAttribute("href") };
+          eleStyle = {
+            backgroundOpacity: eleRgba.slice(
+              eleRgba.length - 5,
+              eleRgba.length - 1
+            ),
+            backgroundColor: eleRgba,
+            backgroundImage: await list[index].getCssValue("background-image"),
+            borderColor: await list[index].getCssValue("background-color"),
+            borderWidth: await list[index].getCssValue("border-width"),
+            borderRadius: await list[index].getCssValue("border-radius"),
+            borderStyle: await list[index].getCssValue("border-style"),
+            shadow: await list[index].getCssValue("box-shadow"),
+          };
+        } else {
+          eleData = { src: await list[index].getAttribute("src") };
+        }
+  
+        pushElementdata(elementRect, eleType, eleData, elementZ, eleStyle);
+        editInnerHtml(innerHtml);
       }
-
-      pushElementdata(elementRect, eleType, eleData, elementZ, eleStyle);
-      editInnerHtml(innerHtml);
+      
+    } catch (error) {
+      console.log('elementsPush error: '+error);
     }
   }
 
@@ -117,13 +117,22 @@ const runAuto = async (url) => {
   await elementsPush(btnList);
 
   return siteObj;
+  } catch (error) {
+    console.log("run auto func error: "+error);
+  }
+  
 };
 
 app.use(express.json());
 
 app.post("/", async (req, res) => {
-  const response = await runAuto(req.body.url);
-  res.send(response);
+  try {
+    const response = await runAuto(req.body.url);
+    res.send(response);
+    
+  } catch (error) {
+    console.log("post request error: "+error);
+  }
 });
 
 app.listen("8080", () => {
